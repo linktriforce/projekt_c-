9002646883b8d63a17a295e503faf94dd4631897
linktriforce <54026512+linktriforce@users.noreply.github.com>
2024-12-09 14:45:13 +0100

    main now work as expected

--- main.cc
+++ main.cc
@@ -11,116 +11,65 @@ using std::string;
 using size_type = string::size_type;
 using letters_and_indices = std::map<size_type, string>;
 
-const string inputFile = "textfiles/fiveLetterWords.txt";
-
-void handleGreenTiles(letters_and_indices &greenTiles)
+letters_and_indices build_list(const std::string &line)
 {
-    string green;
-    std::getline(std::cin, green);
+    letters_and_indices map;
+    std::stringstream ss(line);
 
-    std::stringstream ss(green);
-    
     int index;
     while (ss)
     {
         if (ss >> index)
         {
-            std::cout << index;
             ss.ignore(); // skip space
             char letter[2];
             if (ss >> letter)
             {
-                std::cout << letter;
-                greenTiles.insert({index, string(letter)}); // Type conversion. Safe?
+                map.insert({index, string(letter)}); // Type conversion. Safe?
             }
             ss.ignore(); // skip space
         }
     }
+
+    for (const auto &pair : map)
+    {
+        std::cout << "Index: " << pair.first << ", Value: " << pair.second << std::endl;
+    }
+
+    return map;
 }
 
-void handleYellowTiles(letters_and_indices &yellowTiles)
+std::tuple<std::string, letters_and_indices, letters_and_indices> prompt()
 {
-    int index;
-    string yellow;
-    std::cin >> yellow;
-    std::stringstream ss(yellow);
-    while (ss)
-    {
-        int index;
-        if (ss >> index)
-        {
-            ss.ignore(); // skip space
-            char n[50];
-            if (ss >> n)
-            {
-                yellowTiles.insert({index, string(n)}); // Type conversion. Safe?
-            }
-            ss.ignore(); // skip space
-        }
-    }
+    std::string wrong;
+    std::cout << "enter wrong letters:\n";
+    std::getline(std::cin, wrong);
+
+    std::string correct;
+    std::cout << "enter correct letters (letter index)* e.g '2 c 3 d':\n";
+    std::getline(std::cin, correct);
+    auto corr = build_list(correct);
+
+    std::string misplaced;
+    std::cout << "enter misplaced letters (letter index)* e.g '2 c 3 d':\n";
+    std::getline(std::cin, misplaced);
+    auto misp = build_list(misplaced);
+
+    return {wrong, corr, misp};
 }
+
+const string inputFile = "textfiles/fiveLetterWords.txt";
 
 int main()
 {
-    // Handle gray tiles
-    std::ifstream input(inputFile);
-    string gray;
-    std::cout << "Enter gray tiles: ";
-    std::cin >> gray;
 
-    // Handle green tiles
-    std::cout << "Enter green tiles (e.g format: '2 c 3 d'): ";
-    letters_and_indices greenTiles;
-    handleGreenTiles(greenTiles);
-
-    for (const auto &pair : greenTiles)
-    {
-        std::cout << "Index: " << pair.first << ", Value: " << pair.second << std::endl;
-    }
-
-    // Handle yellow tiles
-    string yellow;
-    std::cout << "Enter yellow tiles (e.g format: 4 efg): ";
-    std::cin >> yellow;
-    letters_and_indices yellowTiles;
-    handleYellowTiles(yellowTiles);
-
-    for (const auto &pair : yellowTiles)
-    {
-        std::cout << "Index: " << pair.first << ", Value: " << pair.second << std::endl;
-    }
+    auto tiles = prompt();
 
     // Instanciate functors
-    invalid_letters_fn invalid_letters_fn(gray); // user input
-    correct_fn correct_fn(greenTiles);           // user input
-    misplaced_fn misplaced_fn(yellowTiles);      // user input
+    invalid_letters_fn invalid_letters_fn(std::get<0>(tiles)); // user input
+    correct_fn correct_fn(std::get<1>(tiles));           // user input
+    misplaced_fn misplaced_fn(std::get<2>(tiles));      // user input
 
     std::set<string> possible_words;
-
-    for (string word; std::getline(input, word);)
-    {
-        if (invalid_letters_fn(word) && correct_fn(word) && misplaced_fn(word)) // from fiveletterwordlist
-        {
-            // If not invalid letter add to set of possible words
-            std::cout << "Words without the letters '" << gray << "' added to the set";
-            possible_words.insert(word);
-        }
-    }
-
-    // map.insert({0, "a"});
-    // map.insert({1, "b"});
-    // map.insert({2, "k"});
-
-    // correct_fn correct_fn(map);
-
-    // if (correct_fn("abcde"))
-    // {
-    //     std::cout << "Doesnt contain a gray letter";
-    // }
-    // else
-    // {
-    //     std::cout << "Contains a gray letter\n";
-    // }
     return 0;
-
 }
