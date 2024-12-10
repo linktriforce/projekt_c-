94ba720e552a73b5797dccf772917757c12022c7
Ludwig Sinclair <ludwig.havasi@gmail.com>
2024-12-10 14:00:55 +0100

    Added some comments to main.cc

--- main.cc
+++ main.cc
@@ -5,10 +5,12 @@ using std::string;
 
 int main()
 {
+    //Get word list from file from user input path
     std::vector<string> possible_words{promptFileName()};
     
     do
     {
+        //Get user input and filter possible_words
         auto tiles = prompt();
         exclude_word_fn o(std::get<0>(tiles), std::get<1>(tiles), std::get<2>(tiles));
         possible_words.erase(std::remove_if(possible_words.begin(), possible_words.end(), o), possible_words.end());
@@ -23,6 +25,7 @@ int main()
         {
             std::cout << w << "\n";
         }
-    } while (anotherIteration());
+    } while (anotherIteration()); //Ask if user wants to continue
+
     return 0;
 }
